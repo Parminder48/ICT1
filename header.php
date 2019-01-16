e6cc254f3f93d4453df8ba9d801a27037100d92a
Parminder48 <45613883+Parminder48@users.noreply.github.com>
2019-01-16 17:17:34 +1000

    Parminder

---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Brow Salon</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body>
+
+  
+  <div class="slider1">
+    <div class="load1">
+
+    </div>
+    <div class="content1">
+      <div class="principal1">
+      
+    </div>
+
+  </div>
+  <nav class="navbar navbar-expand-lg navbar-light">
+          <div class="hedder-up">
+            <h1 ><a href="home.php" class="navbar-brand" data-blast="color"><img src="images/logo.png" style="width: 380px;background-size: cover;height: 109px;margin-top: -3%;"></a></h1>
+          </div>
+         
+          <div class="headdser-nav-color" data-blast="bgColor">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+              <ul class="navbar-nav ">
+                <li class="nav-item active">
+                  <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a href="about.php" class="nav-link" >About Us</a>
+                </li>
+                <li class="nav-item">
+                  <a href="product.php" class="nav-link">Product</a>
+                </li>
+                <li class="nav-item">
+                  <a href="service.php" class="nav-link">Service</a>
+                </li>
+                <li class="nav-item">
+                  <a href="contact.php" class="nav-link">CONTACT</a>
+                </li>
+                <li class="nav-item">
+                  <a href="signup.php" class="nav-link">SIGN UP</a>
+                </li>
+                
+              </ul>
+            </div>
+          </div>
+        </nav>
+      
+
+        <!--//navigation section -->
+        <div class="clearfix"> </div>
+      </div>
+     
+</body>
+</html>
