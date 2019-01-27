c8bb28a99b59db6b246d47848050f60bd65ba5a9
DeeptiMalik <45705926+DeeptiMalik@users.noreply.github.com>
2019-01-27 17:19:43 +1100

    Add files via upload

---
+++ about.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Brow Salon</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+
+</head>
+<style type="text/css">
+	
+	 body .package {
+
+    width: 100%;
+    padding: 5%;
+    background-color: white opacity:0.5%;
+    height: 1800px;
+    display: block;
+    position: relative;
+
+  }
+  .package .col-sm-11{
+
+display: inline-block;
+  width: 90%;
+  height: auto;
+  float: left;
+  clear: right;
+  border: 1px solid white;
+  border-radius: 5px;
+  box-shadow: 1px 5px 11px 3px lightblue;
+  background-color: white;
+  margin-left:5%;
+  margin-top: 3%;
+  text-align: center;
+  padding:0px; 
+
+
+
+}
+
+.package .col-sm-11:hover{
+
+   -webkit-transition: all 1s;
+    -moz-transition: all 1s;
+    transition: all 1s;
+  -o-transition: all 1s ease-in-out;
+  -ms-transition: all 1s ease-in-out;
+    -webkit-box-shadow: 15px 15px 40px 15px #aaa;
+    -moz-box-shadow: 15px 15px 40px 15px #aaa;
+    box-shadow: 15px 15px 40px 15px lightblue;
+}
+.w3l_header {
+    font-size: 3em;
+    color: #262c38;
+    letter-spacing: 1px;
+    position: relative;
+    font-weight: 600;
+    text-align: center;
+}
+.w3l_header span {
+    color: #545151;
+    font-weight: 200;
+}
+.w3l_header:after {
+    border-top: 2px solid blue;
+    display: block;
+    width: 300px;
+    content: "*";
+    margin: 8px auto 0;
+}
+.wthree_head_section p {
+    width: 100%;
+    text-align: center;
+    font-size: 2em;
+}
+.wthree_head_section{
+	margin-bottom:4em;
+}
+
+
+.area-img1, .area-img2, .area-img3, .area-img4 {
+    background:no-repeat 0px 0px;
+}
+.area-img1, .area-img2, .area-img3, .area-img4 {
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    -ms-background-size: cover;
+    background-size: cover;
+    min-height: 330px;
+}
+.area-img1, .area-img2, .area-img3, .area-img4, .area-right {
+    flex-basis: 50%;
+    -webkit-flex-basis: 50%;
+}
+
+</style>
+<?php include('header.php');?>
+<body>
+<div class="package">
+  <div class="col-sm-11">
+				  <div class="container">
+						    <div class="wthree_head_section">
+						      <h3 class="w3l_header">About <span>Us</span></h3>
+						      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+						    </div>  
+				     </div>
+
+		
+ </div>
+</div>
+
+
+
+</body>
+</html>
